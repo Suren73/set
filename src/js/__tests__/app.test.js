e68da4df005dd7b58f78abcbea5fac2c6d24abeb
Andrei Surkov <andronsurkov73@gmail.com>
2021-03-25 22:10:40 +0300

    Добавил выполненное Домашнее задание

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,112 @@
+import Team from '../app';
+import Bowerman from '../bowerman';
+import Daemon from '../daemon';
+import Undead from '../undead';
+import Character from '../character';
+
+test('testing class Team', () => {
+	const expected = [
+		{
+			name: 'Andrei',
+			type: 'Bowman',
+			health: 100,
+			level: 1,
+			attack: 25,
+			defence: 25,
+		},
+	];
+	const team = new Team();
+	const bowman = new Bowerman('Andrei');
+	team.add(bowman);
+	expect([...team.members]).toEqual(expected);
+});
+
+test('testing class Team', () => {
+	expect(() => {
+		const team = new Team();
+		const bowman = new Bowerman('Andrei');
+		team.add(bowman);
+		team.add(bowman);
+	}).toThrow();
+});
+
+test('testing class Team', () => {
+	const expected = [
+		{
+			name: 'Andrei',
+			type: 'Bowman',
+			health: 100,
+			level: 1,
+			attack: 25,
+			defence: 25,
+		},
+		{
+			name: 'Sergei',
+			type: 'Daemon',
+			health: 100,
+			level: 1,
+			attack: 10,
+			defence: 40,
+		},
+		{
+			name: 'Denis',
+			type: 'Undead',
+			health: 100,
+			level: 1,
+			attack: 25,
+			defence: 25,
+		}
+	];
+	const team = new Team();
+	const bowman = new Bowerman('Andrei');
+	const daemon = new Daemon('Sergei');
+	const undead = new Undead('Denis');
+	team.addAll(bowman, daemon, undead, bowman);
+	expect([...team.members]).toEqual(expected);
+});
+
+test('testing class Team', () => {
+	const expected = [
+		{
+			name: 'Andrei',
+			type: 'Bowman',
+			health: 100,
+			level: 1,
+			attack: 25,
+			defence: 25,
+		},
+	];
+	const team = new Team();
+	const bowman = new Bowerman('Andrei');
+	team.add(bowman);
+	expect(team.toArray()).toEqual(expected);
+});
+
+test('testing the player creation function', () => {
+	const expected = {
+		name: 'Andrei',
+		type: 'Bowman',
+		health: 100,
+		level: 1,
+		attack: 0,
+		defence: 0,
+	};
+	const received = new Character('Andrei', 'Bowman');
+	expect(received).toEqual(expected);
+});
+
+test('testing the player creation function', () => {
+	expect(() => new Character('Andrei', 10)).toThrow('Ошибка типа игрока');
+});
+
+test('testing the player creation function', () => {
+	expect(() => new Character('Andrei', 'Temp')).toThrow('Ошибка типа игрока');
+});
+
+test('testing the player creation function', () => {
+	expect(() => new Character('A', 'Bowman')).toThrow('Ошибка имени');
+});
+
+test('testing the player creation function', () => {
+	expect(() => new Character('Andreiiiiii', 'Bowman')).toThrow('Ошибка имени');
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,37 @@
+// export default const Character = require('./character');
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
 
+  add(persona) {
+    if (!this.members.has(persona)) {
+      this.members.add(persona);
+    } else {
+      throw new Error('Персонаж уже добавлен');
+    }
+  }
+
+  addAll(...personas) {
+    personas.forEach((persona) => this.members.add(persona));
+  }
+
+  toArray() {
+    return [...this.members];
+  }
+}
+// const bowman = new Character('Andrei', 'Bowman', 100, 1, 25, 25);
+// const swordsman = new Character('Andrei', 'Swordsman', 100, 1, 25, 25);
+// const magician = new Character('Andrei', 'Magician', 100, 1, 25, 25);
+
+// const team = new Team;
+// console.log(team);
+// console.log(team.members.size);
+// team.add(bowman);
+
+// console.log(team.members);
+// let arr = team.toArray();
+// console.log(arr);
+
+// team.addAll(bowman, magician);
+// console.log(team.members);
